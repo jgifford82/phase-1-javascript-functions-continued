a38e10e2613bb0857d6345ea8082bdb6f24f2d67
Jennifer Gifford <jgifford82@gmail.com>
2022-05-20 18:47:15 -0600

    commit tree 97136423fd3e6b7dcb587cf317a3f48419a73e52

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+// This is a function expression (or anonymous function). 
+// Rather than a named function (function declaration), it's defined with a variable.
+// The variable name is a pointer that can be used to invoke or call the anonymous function.
+const mondayWork = function(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+// function wrapAdjective(flair = "*") {
+//     const prompt = function(adj = "special") {
+//         return `You are ${adj}!`
+//     }
+// }
+
+function wrapAdjective(flair = "*") {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
